b994d18234cd817869fc3dc9a92b4dcb7679f053
HappyPotato5 <tucalvitasexy@gmail.com>
2026-09-16 19:24:11 +0200

    Prepared for publishing in crates.io

--- serax_macros/src/lib.rs
+++ serax_macros/src/lib.rs
@@ -1,4 +1,11 @@
-#![doc(hidden)]
+
+//! The crate that implements all Serax' proc-macros.
+//! 
+//! <div class="warning">
+//! You should never add this crate directly to your dependencies.
+//! 
+//! This crate is only meant to be used by Serax.
+//! </div>
 
 mod type_;
 mod serialize;
@@ -10,11 +17,13 @@ use proc_macro::TokenStream;
 use serialize::*;
 use deserialize::*;
 
+/// Macro that implements `serax::Serialize`.
 #[proc_macro_derive(Serialize)]
 pub fn serialize(input: TokenStream) -> TokenStream {
     serialize_impl(input)
 }
 
+/// Macro that implements `serax::Deserialize`.
 #[proc_macro_derive(Deserialize)]
 pub fn deserialize(input: TokenStream) -> TokenStream {
     deserialize_impl(input)
